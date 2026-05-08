e73e6a04f77fcb3ed8c3e7cd492efa3984e9b8a0
Tobias Andresen <tiltobias@gmail.com>
2026-05-08 17:50:17 +0200

    cleaner riskTree

--- frontend/src/components/RiskTree.tsx
+++ frontend/src/components/RiskTree.tsx
@@ -1,4 +1,5 @@
 import useDataStore from "../hooks/useDataStore";
+import "./RiskTree.css";
 
 function RiskTree() {
 
@@ -22,33 +23,46 @@ function RiskTree() {
   }        
 
   return (
-    <ul>
-      <li><strong>Kommune:</strong> {currentKommune.name} ({selectedKommune})</li>
-      <li><strong>Risk:</strong> {currentKommuneCache.totalRisk.toFixed(3) ?? "-"}
-        <ul>
-          {dataModel.elements.map(element => (
-            <li key={element.key}>
-              <input type="checkbox" checked={!element.disabled} onChange={(e) => {
+    <div className="risk-tree">
+      <strong>Total Risk</strong>
+      <ul>
+        {dataModel.elements.map(element => (
+          <li key={element.key}>
+            <input 
+              type="checkbox" 
+              checked={!element.disabled} 
+              onChange={(e) => {
                 element.disabled = !e.target.checked;
                 refreshCacheRisk(); //TODO: Check if a metric has changed while disabled
-              }} />
-              <strong>{element.name}:</strong> {currentKommuneCache[element.key]?.toFixed(0) ?? "-"}
-              
-              <ul>
-                {element.metrics.map(metric => (
-                  <li key={metric.key}>
-                    <input type="checkbox" checked={!metric.disabled} onChange={(e) => {
+              }} 
+              id={`risktree-${element.key}`}
+            />
+            <label htmlFor={`risktree-${element.key}`}>
+              <strong>{element.name}</strong>
+            </label>
+            
+            <ul>
+              {element.metrics.map(metric => (
+                <li key={metric.key}>
+                  <input 
+                    type="checkbox" 
+                    checked={!metric.disabled} 
+                    onChange={(e) => {
                       metric.disabled = !e.target.checked;
                       refreshCacheElement(element.key); // Update only this element's value
-                    }} />
-                    <strong>{metric.name}:</strong> {currentKommune[metric.key]?.toFixed(0) ?? "-"}</li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </li>
-    </ul>
+                    }}
+                    id={`risktree-${element.key}-${metric.key}`}
+                  />
+                  <label htmlFor={`risktree-${element.key}-${metric.key}`}>
+                    {metric.name}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
