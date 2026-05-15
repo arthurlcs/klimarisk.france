e0dd6122ab84233fd84bbf7a914b8823135ba261
Tobias Andresen <tiltobias@gmail.com>
2026-05-15 14:21:59 +0200

    styled riskTree with tooltips

--- frontend/src/components/RiskTree.tsx
+++ frontend/src/components/RiskTree.tsx
@@ -1,6 +1,7 @@
 import useDataStore from "../hooks/useDataStore";
 import "./RiskTree.css";
 import useLanguageStore, { t } from "../hooks/useLanguageStore";
+import Tooltip from "./Tooltip";
 
 function RiskTree() {
 
@@ -19,42 +20,64 @@ function RiskTree() {
   }        
 
   return (
-    <div className="risk-tree">
-      <strong>{l(t.common.totalRisk)}</strong>
+    <div className="riskTree">
+      {/* <div className="treeHandle">
+        {l(t.common.totalRisk)}
+      </div> */}
       <ul>
         {dataModel.elements.map(element => (
           <li 
             key={element.key}
             onMouseEnter={() => setHighlightedDistribution({type: "element", key: element.key})}
             onMouseLeave={() => setHighlightedDistribution(null)}
+            className={`treeElement ${element.disabled ? "disabled" : ""}`}
           >
-            <input 
-              type="checkbox" 
-              checked={!element.disabled} 
-              onChange={(e) => {
-                element.disabled = !e.target.checked;
-                refreshCacheRisk(); //TODO: Check if a metric has changed while disabled
-              }} 
-              id={`risktree-${element.key}`}
-            />
-            <label htmlFor={`risktree-${element.key}`}>
-              <strong>{l(element.name)}</strong>
+            <label 
+              htmlFor={`risktree-${element.key}`}
+              className="treeHandle"
+            >
+              <input 
+                type="checkbox" 
+                checked={!element.disabled} 
+                onChange={(e) => {
+                  element.disabled = !e.target.checked;
+                  refreshCacheRisk(); //TODO: Check if a metric has changed while disabled
+                }} 
+                id={`risktree-${element.key}`}
+                className="treeBox"
+              />
+              <div className="treeName">
+                <Tooltip text={l(element.description)}>
+                  {l(element.name)}
+                </Tooltip>
+              </div>
             </label>
-            
+
             <ul>
               {element.metrics.map(metric => (
-                <li key={metric.key}>
-                  <input 
-                    type="checkbox" 
-                    checked={!metric.disabled} 
-                    onChange={(e) => {
-                      metric.disabled = !e.target.checked;
-                      refreshCacheElement(element.key); // Update only this element's value
-                    }}
-                    id={`risktree-${element.key}-${metric.key}`}
-                  />
-                  <label htmlFor={`risktree-${element.key}-${metric.key}`}>
-                    {l(metric.name)}
+                <li 
+                  key={metric.key}
+                  className={`treeMetric ${element.disabled || metric.disabled ? "disabled" : ""}`}
+                >
+                  <label 
+                    htmlFor={`risktree-${element.key}-${metric.key}`}
+                    className="treeHandle"
+                  >
+                    <input 
+                      type="checkbox" 
+                      checked={!metric.disabled} 
+                      onChange={(e) => {
+                        metric.disabled = !e.target.checked;
+                        refreshCacheElement(element.key); // Update only this element's value
+                      }}
+                      id={`risktree-${element.key}-${metric.key}`}
+                      className="treeBox"
+                    />
+                    <div className="treeName">
+                      <Tooltip text={l(metric.description)}>
+                        {l(metric.name)}
+                      </Tooltip>
+                    </div>
                   </label>
                 </li>
               ))}
